ad41e17eca6a23400e9e2a4a89e4beafa219fde3
Pablo Aquise <151467555+PabloAquiseLozano@users.noreply.github.com>
2025-09-02 14:53:46 -0500

    added redirection to wsp for missing links (#39)

--- apps/web-client/src/components/ui/ProductCard.tsx
+++ apps/web-client/src/components/ui/ProductCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Product } from "@/data-list/products";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 interface Props {
   product: Product;
@@ -12,7 +12,10 @@ export const ProductCard = ({ product }: Props) => {
   return (
     <article
       key={product.model}
-      className={twMerge("keen-slider__slide flex flex-col h-full group rounded-2xl p-5 shadow-sm hover:shadow-md border-solid border-1 border-gray-400/40 bg-white",`${product?.classification === "premium" && "border-primary"}`)}
+      className={twMerge(
+        "keen-slider__slide flex flex-col h-full group rounded-2xl p-5 shadow-sm hover:shadow-md border-solid border-1 border-gray-400/40 bg-white",
+        `${product?.classification === "premium" && "border-primary"}`,
+      )}
     >
       <div className="mb-3">
         <div className="relative w-full h-48 rounded-xl overflow-hidden bg-white">
@@ -148,6 +151,7 @@ export const ProductCard = ({ product }: Props) => {
       <div className="mt-3 grid gap-2 grid-cols-[1fr_auto] row-span-1 items-end">
         <a
           href={`https://wa.me/51972300301?text=Hola%20iubizon,%20me%20interesa%20el%20${product.type}%20${product.name}`}
+          target="_blank"
           className="w-full rounded-xl px-4 py-2 text-center text-sm font-semibold shadow-sm transition bg-secondary text-white"
         >
           Contactar para comprar
