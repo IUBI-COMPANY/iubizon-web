4b04bc0f3b6216959088dc69be07145ab8d1fcbe
Pablo Aquise Lozano <aquise.pablo@iestpgildaballivian.edu.pe>
2025-09-24 16:54:57 -0500

    Add Alert component and enhance SupportInformation form with visit date, time, and address fields

--- apps/web-client/src/app/repairs/SupportInformation.tsx
+++ apps/web-client/src/app/repairs/SupportInformation.tsx
@@ -1,14 +1,16 @@
+import React from "react";
 import { ObjectSchema } from "yup";
 import * as yup from "yup";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useFormUtils } from "@/hooks/useFormUtils";
-import { twMerge } from "tailwind-merge";
-import React from "react";
 import { Form } from "@/components/ui/Form";
-import { Switch } from "@/components/ui/Switch";
 import { RadioGroup } from "@/components/ui/RadioGroup";
 import { Button } from "@/components/ui/Button";
+import { TimePicker } from "@/components/ui/TimePicker";
+import { DatePicker } from "@/components/ui/DatePicker";
+import { Input } from "@/components/ui/Input";
+import { Alert } from "@/components/ui/Alert";
 
 interface Props {
   repairFormData: object;
@@ -19,6 +21,12 @@ interface Props {
 
 interface SupportFormData {
   visitType: string;
+  visitDate: string;
+  visitTime: string;
+  district?: string;
+  address?: string;
+  department?: string;
+  province?: string;
 }
 
 export const SupportInformation = ({
@@ -29,6 +37,28 @@ export const SupportInformation = ({
 }: Props) => {
   const schema: ObjectSchema<SupportFormData> = yup.object({
     visitType: yup.string().required(),
+    visitDate: yup.string().required(),
+    visitTime: yup.string().required(),
+    district: yup.string().when("visitType", {
+      is: "house",
+      then: (schema) => schema.required(),
+      otherwise: (schema) => schema.notRequired(),
+    }),
+    address: yup.string().when("visitType", {
+      is: "house",
+      then: (schema) => schema.required(),
+      otherwise: (schema) => schema.notRequired(),
+    }),
+    department: yup.string().when("visitType", {
+      is: "shipping",
+      then: (schema) => schema.required(),
+      otherwise: (schema) => schema.notRequired(),
+    }),
+    province: yup.string().when("visitType", {
+      is: "shipping",
+      then: (schema) => schema.required(),
+      otherwise: (schema) => schema.notRequired(),
+    }),
   });
 
   const {
@@ -38,11 +68,17 @@ export const SupportInformation = ({
     watch,
   } = useForm<SupportFormData>({
     resolver: yupResolver(schema),
+    defaultValues: {
+      visitType: "local",
+    },
   });
 
-  const repairVisitType = watch("visitType");
-
   const { required, error, errorMessage } = useFormUtils({ errors, schema });
+
+  const isLocalVisit = watch("visitType") === "local";
+  const isHouseVisit = watch("visitType") === "house";
+  const isShipping = watch("visitType") === "shipping";
+
   const onSubmit = (formData: SupportFormData) => {
     setRepairFormData({ ...repairFormData, ...formData });
   };
@@ -68,28 +104,176 @@ export const SupportInformation = ({
                       onChange={onChange}
                       options={[
                         {
-                          label: "Realizar visita a nuestro local",
+                          label: "Quiero ir al local",
                           value: "local",
                         },
-                        { label: "Visita técnica a domicilio", value: "house" },
+                        {
+                          label: "Quiero una visita técnica en mi domicilio",
+                          value: "house",
+                        },
+                        {
+                          label: "Quiero enviar mi producto al local",
+                          value: "shipping",
+                        },
                       ]}
                     />
                   )}
                 />
+
+                {isLocalVisit && (
+                  <div className="w-full grid gap-6 my-6">
+                    <p className="text-base text-secondary mt-2">
+                      Puedes traer tu equipo a nuestro local ubicado en Psje Los
+                      Jazmines 121, Chorrillos. Lunes a Viernes de 9:00 AM a
+                      6:00 PM y sábados de 10:00 AM a 4:00 PM.
+                    </p>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d344.71337159212857!2d-77.01877893930197!3d-12.181314674423286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b77bc923c7dd%3A0xc9a1cab224d7580f!2sPje.%20los%20Jazmines%20121%2C%20Chorrillos%2015066!5e0!3m2!1ses!2spe!4v1758727743550!5m2!1ses!2spe"
+                      width="600"
+                      height="450"
+                      allowFullScreen={true}
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                      className="w-full "
+                    ></iframe>
+                  </div>
+                )}
+                {isHouseVisit && (
+                  <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 my-6">
+                    <div className="sm:col-span-1">
+                      <Controller
+                        name="visitDate"
+                        control={control}
+                        render={({ field: { onChange, value, name } }) => (
+                          <DatePicker
+                            label="Dinos qué día podemos visitarte"
+                            name={name}
+                            value={value}
+                            error={error(name)}
+                            helperText={errorMessage(name)}
+                            required={required(name)}
+                            onChange={onChange}
+                          />
+                        )}
+                      />
+                    </div>
+                    <div className="sm:col-span-1">
+                      <Controller
+                        name="visitTime"
+                        control={control}
+                        render={({ field: { onChange, value, name } }) => (
+                          <TimePicker
+                            label="Dinos a qué hora podemos visitarte"
+                            name={name}
+                            value={value}
+                            error={error(name)}
+                            helperText={errorMessage(name)}
+                            required={required(name)}
+                            onChange={onChange}
+                          />
+                        )}
+                      />
+                    </div>
+                  </div>
+                )}
+                {isShipping && (
+                  <div className="grid grid-cols-1 gap-x-4 sm:grid-cols-2 my-6">
+                    <Alert
+                      type="info"
+                      message="Estos datos servirán para el envío de su equipo una vez terminado el servicio."
+                    />
+                    <div className="sm:col-span-1">
+                      <Controller
+                        name="department"
+                        control={control}
+                        render={({ field: { onChange, value, name } }) => (
+                          <Input
+                            label="Departamento"
+                            placeholder="Ej. Lima"
+                            name={name}
+                            value={value}
+                            error={error(name)}
+                            helperText={errorMessage(name)}
+                            required={required(name)}
+                            onChange={onChange}
+                          />
+                        )}
+                      />
+                    </div>
+                    <div className="sm:col-span-1">
+                      <Controller
+                        name="province"
+                        control={control}
+                        render={({ field: { onChange, value, name } }) => (
+                          <Input
+                            label="Provincia"
+                            placeholder="Ej. Lima"
+                            name={name}
+                            value={value}
+                            error={error(name)}
+                            helperText={errorMessage(name)}
+                            required={required(name)}
+                            onChange={onChange}
+                          />
+                        )}
+                      />
+                    </div>
+                  </div>
+                )}
+                {(isHouseVisit || isShipping) && (
+                  <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-6 my-6">
+                    <div className="sm: col-span-2">
+                      <Controller
+                        name="district"
+                        control={control}
+                        render={({ field: { onChange, value, name } }) => (
+                          <Input
+                            label="Distrito"
+                            placeholder="Ej. Chorrillos"
+                            name={name}
+                            value={value}
+                            error={error(name)}
+                            helperText={errorMessage(name)}
+                            required={required(name)}
+                            onChange={onChange}
+                          />
+                        )}
+                      />
+                    </div>
+                    <div className="sm: col-span-4">
+                      <Controller
+                        name="address"
+                        control={control}
+                        render={({ field: { onChange, value, name } }) => (
+                          <Input
+                            label="Dirección"
+                            placeholder="Av. Ejemplo 123"
+                            name={name}
+                            value={value}
+                            error={error(name)}
+                            helperText={errorMessage(name)}
+                            required={required(name)}
+                            onChange={onChange}
+                          />
+                        )}
+                      />
+                    </div>
+                  </div>
+                )}
+                <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 gap-3">
+                  <Button
+                    block
+                    variant="secondary"
+                    type="button"
+                    onClick={() => setCurrentStep(currentStep - 1)}
+                  >
+                    Atrás
+                  </Button>
+                  <Button block variant="primary" type="submit">
+                    {currentStep === 2 ? "Finalizar" : "Siguiente"}
+                  </Button>
+                </div>
               </div>
-            </div>
-            <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 gap-3">
-              <Button
-                block
-                variant="secondary"
-                type="button"
-                onClick={() => setCurrentStep(currentStep - 1)}
-              >
-                Atrás
-              </Button>
-              <Button block variant="primary" type="submit">
-                {currentStep === 2 ? "Finalizar" : "Siguiente"}
-              </Button>
             </div>
           </div>
         </Form>
