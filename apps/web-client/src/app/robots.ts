3d7765feee0203bc0f1fbf7aac602d28e10b9dba
PabloAquiseLozano <aquise.pablo@iestpgildaballivian.edu.pe>
2025-12-09 11:35:33 -0500

    formated with prettier

--- apps/web-client/src/app/robots.ts
+++ apps/web-client/src/app/robots.ts
@@ -1,24 +1,24 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: [
-            {
-                userAgent: "Googlebot",
-                allow: "/",
-                disallow: ["/api/"],
-            },
-            {
-                userAgent: "Googlebot-Image",
-                allow: "/",
-                disallow: ["/api/"],
-            },
-            {
-                userAgent: "*",
-                allow: "/",
-                disallow: ["/api/"],
-            },
-        ],
-        sitemap: "https://www.iubizon.com/sitemap.xml",
-    };
+  return {
+    rules: [
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: ["/api/"],
+      },
+      {
+        userAgent: "Googlebot-Image",
+        allow: "/",
+        disallow: ["/api/"],
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/"],
+      },
+    ],
+    sitemap: "https://www.iubizon.com/sitemap.xml",
+  };
 }
